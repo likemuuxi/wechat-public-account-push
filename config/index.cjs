fc5041a2610aac2e06f6fa316c29f74aa217b4d9
JakeYang <85612715+likemuuxi@users.noreply.github.com>
2024-12-06 22:08:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,62 +6,44 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
-  PROVINCE: '广东',
-  CITY: '惠州',
-
-  USERS: [
+    "APP_ID":"wx72eb1cb4e238912a",
+    "APP_SECRET":"a681ffbb7521ac6d7e82ab8878c52019",
+    "IS_SHOW_COLOR":true,
+    "CALLBACK_TEMPLATE_ID":"2",
+    "CALLBACK_USERS":[{"name":"自己","id":"1"}],
+    "USERS":[
     {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+      "name":"小楠",
+      "id":"2",
+      "useTemplateId":"1",
+      "province":"福建省",
+      "city":"厦门市",
+      "horoscopeDate":"05-26",
+      "horoscopeDateType":"今日",
+      "openUrl":"https://www.muuxi.cn/",
+      "festivals":[
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-    }
-  ],
-
+          "type":"节日",
+          "name":"情人节",
+          "date":"02-14",
+          "year":"2025"
+        }],
+     "customizedDateList":[
+       {"keyword":"相识","date":"2022-09-16"},
+       {"keyword":"相恋","date":"2022-10-09"}]
+    }],
+    "SWITCH":{
+    "weather":true,
+      "holidaytts":true,
+      "CIBA":true,
+      "oneTalk":false,
+      "earthyLoveWords":false,
+      "momentCopyrighting":false,
+      "poisonChickenSoup":false,
+      "poetry":false,
+      "horoscope":false,
+      "birthdayMessage":true
+  }
 }
 
 module.exports = USER_CONFIG
-
